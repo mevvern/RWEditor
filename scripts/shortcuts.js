d2663b26ede98ff079b59602d90d4cb9d494c4b9
mev <32826289+mevvern@users.noreply.github.com>
2023-07-28 12:38:08 -0700

    completed save/load functions
    save function now saves the level attributes as well as the level itself

--- scripts/shortcuts.js
+++ scripts/shortcuts.js
@@ -16,16 +16,29 @@ function paste(event) {
 function save(event) {
 	if (event.ctrlKey && event.key === 's') {
 		event.preventDefault();
-		localStorage.setItem('levelArray', JSON.stringify(levelArray))
+		localStorage.setItem('levelArray', JSON.stringify(levelArray));
+		localStorage.setItem('tilesPerRow', tilesPerRow);
+		localStorage.setItem('tilesPerColumn', tilesPerColumn);
+		localStorage.setItem('playStartX', playStartX);
+		localStorage.setItem('playStartY', playStartY);
+		localStorage.setItem('playEndX', playEndX);
+		localStorage.setItem('playEndY', playEndY);
 		console.log('saved the level');
 	}
 };
 
 function load(event) {
 	if (event.ctrlKey && event.key === 'l') {
+		event.preventDefault()
 		levelArray = JSON.parse(localStorage.getItem('levelArray'))
-		console.log('loaded the level :3')
+		tilesPerRow = parseInt(localStorage.getItem('tilesPerRow'))
+		tilesPerColumn = parseInt(localStorage.getItem('tilesPerColumn'));
+		playStartX = parseInt(localStorage.getItem('playStartX'));
+		playStartY = parseInt(localStorage.getItem('playStartY'));
+		playEndX = parseInt(localStorage.getItem('playEndX'));
+		playEndY = parseInt(localStorage.getItem('playEndY'));
 		drawVisLevel()
+		console.log('loaded the level :3')
 	} 
 }
 
